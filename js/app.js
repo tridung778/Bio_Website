c0d2f1a29945c295f468305f97b187d4334bea20
tridung778 <crytv778@gmail.com>
2024-04-03 09:59:50 +0700

    dung commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var video = document.getElementById("myVideo");
-  video.play();
+var video = document.getElementById("myVideo");
+var playButton = document.querySelector(".play-button");
+var icon = document.getElementById("icon");
+
+document.addEventListener("DOMContentLoaded", () => {
+  icon.classList.add("fa-play");
+});
+
+playButton.addEventListener("click", function () {
+  if (video.paused) {
+    video.play();
+    icon.classList.remove("fa-play");
+    icon.classList.add("fa-pause");
+  } else {
+    video.pause();
+    icon.classList.remove("fa-pause");
+    icon.classList.add("fa-play");
+  }
+});
+
+video.addEventListener("play", function () {
+  icon.classList.remove("fa-play");
+  icon.classList.add("fa-pause");
+});
+
+video.addEventListener("pause", function () {
+  icon.classList.remove("fa-pause");
+  icon.classList.add("fa-play");
 });
