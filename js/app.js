4e704d848c54eda6deef86c9a5d81e3a406dafef
tridung778 <crytv778@gmail.com>
2024-04-03 18:57:29 +0700

    dung commit

--- js/app.js
+++ js/app.js
@@ -27,3 +27,19 @@ video.addEventListener("pause", function () {
   icon.classList.remove("fa-pause");
   icon.classList.add("fa-play");
 });
+
+// Open popup
+
+let form_popup = document.getElementById("form_popup");
+let content = document.getElementById("content");
+
+function showPopup() {
+  form_popup.classList.add("form-popup-open");
+
+  content.style.filter = "brightness(0.5) blur(10px)";
+}
+
+function closePopup() {
+  form_popup.classList.remove("form-popup-open");
+  content.style.filter = "none";
+}
